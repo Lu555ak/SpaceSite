c900829a4de384651a59de687081b95aa0834dd8
Lu555ak <grgecicluka@gmail.com>
2022-06-08 22:44:52 +0200

    Added solar system

--- main.js
+++ main.js
@@ -1,16 +1,15 @@
 import './style.css';
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
-import { Vector3 } from 'three';
 
 // Initial Setup
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 const renderer = new THREE.WebGLRenderer({ canvas: document.querySelector('#threeBackground') });
 renderer.setPixelRatio(window.devicePixelRatio);
-renderer.setSize(window.innerWidth, window.innerHeight);
-camera.position.setZ(30);
+renderer.setSize(window.innerWidth, window.innerHeight)
 renderer.render(scene, camera);
+camera.position.setZ(250);
 
 // Light Setup
 const pointLight = new THREE.PointLight(0xffffff);
@@ -23,23 +22,15 @@ const spaceTexture = new THREE.TextureLoader().load('space.jpg');
 scene.background = spaceTexture;
 
 
-const sunTexture = new THREE.TextureLoader().load('Textures/sunTexture.jpg');
-const sun = new THREE.Mesh(
-  new THREE.SphereGeometry(30, 50, 50),
-  new THREE.MeshStandardMaterial({ map: sunTexture })
-);
-sun.position.set(0,0,0);
-scene.add(sun);
-
 class Planet{
-    constructor(size, texture, rotationSpeed, revolutionSpeed, distanceFromParent, parentPosition){
+    constructor(size, texture, rotationSpeed, revolutionSpeed, distanceFromParent){
         var planetTexture = new THREE.TextureLoader().load('Textures/' + texture);
         var planet = new THREE.Mesh(
             new THREE.SphereGeometry(size, 50, 50),
             new THREE.MeshStandardMaterial({ map: planetTexture })
         );
         var ring = new THREE.Mesh( 
-            new THREE.RingGeometry( distanceFromParent, distanceFromParent+0.3, distanceFromParent ),
+            new THREE.RingGeometry( distanceFromParent-0.15, distanceFromParent+0.15, 150 ),
             new THREE.MeshBasicMaterial( { color: 0xffffff, side: THREE.DoubleSide } )
         );
         ring.rotateX(Math.PI / 2);
@@ -49,28 +40,42 @@ class Planet{
         this.distanceFromParent = distanceFromParent;
         this.rotationSpeed = rotationSpeed;
         this.revolutionSpeed = revolutionSpeed;
-        this.parentPosition = parentPosition;
+        //this.parentPosition = parentPosition;
+
         this.time = 0;
     }
 
     animate() {
         this.time += 0.01;
         this.instance.rotation.y += this.rotationSpeed / 1000;
-        this.instance.position.x = this.distanceFromParent*Math.cos(this.time) + 0;
-        this.instance.position.z = this.distanceFromParent*Math.sin(this.time) + 0;
+        this.instance.position.x = this.distanceFromParent*Math.cos(this.time * this.revolutionSpeed); //+ this.parentPosition.x;
+        this.instance.position.z = this.distanceFromParent*Math.sin(this.time * this.revolutionSpeed); //+ this.parentPosition.z;
+        //this.instance.position.y = this.parentPosition.y;
     }
 }
 
-let A = new Planet(5, 'sunTexture.jpg', 5, 5, 50, sun.position);
+
+var planets = [];
+planets.push(new Planet(40, 'sunTexture.jpg', 2.5, 0, 0));
+planets.push(new Planet(5, 'mercuryTexture.jpeg', 2.5, 2, 50));
+planets.push(new Planet(6.5, 'venusTexture.jpeg', -0.5, 1.8, 80));
+planets.push(new Planet(7, 'earthTexture.jpeg', 5, 1.4, 110));
+planets.push(new Planet(6, 'marsTexture.jpg', 4, 1.2, 130));
+planets.push(new Planet(20, 'jupiterTexture.jpeg', 9, 0.8, 180));
+planets.push(new Planet(18, 'saturnTexture.jpeg', 8, 0.6, 220));
+planets.push(new Planet(10, 'neptuneTexture.jpeg', 6, 0.35, 260));
+planets.push(new Planet(10, 'uranusTexture.jpeg', 6.5, 0.15, 290));
 
 
 const controls = new OrbitControls(camera, renderer.domElement);
 function animate() {
     requestAnimationFrame(animate);
-    A.animate();
+    
+    for(var i=0; i<planets.length; i++){
+        planets[i].animate();
+    }
 
     controls.update();
-
     renderer.render(scene, camera);
 }
 
